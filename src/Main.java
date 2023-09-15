0851c9e13a48a937a34f33847ed2a90f5585bf44
kingabreel <gabrielyury441@gmail.com>
2023-09-15 18:03:30 -0300

    simplified Main class, MainWindows now extends JFrame

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import view.MainWindow;
 
 public class Main {
     public static void main(String[] args) {
-        MainWindow gui = new MainWindow();
-
-        gui.window();
+        new MainWindow();
     }
 }
--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -3,36 +3,38 @@ package view;
 import javax.swing.*;
 import java.awt.*;
 
-public class MainWindow {
-    //frame is the main windows, it's constructor receive a parameter that will be the title of the window.
-    public void window(){
-        JFrame frame = new JFrame("First User Interface Program");
-        frame.setSize(500, 200);
+public class MainWindow extends JFrame{
+    public MainWindow(){
+        this.setTitle("First User Interface Program");
+        this.setSize(500, 200);
+        this.setResizable(false);
         /*This method "setDefaultCloseOperation" will tell java what to do when the window is closed, if you do not use
         it, when you close the window the program will continue running.*/
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.setLayout(new FlowLayout());
+        this.setLayout(new FlowLayout());
 
         //JTextField will receive a user input. The argument size of the text field, each unit is the size of one character
         JTextField inputText = new JTextField(30);
 
         JButton okButton = new JButton("Ok");
-
-        frame.add(inputText);
-        frame.add(okButton);
-
         JLabel label = new JLabel();
+        this.add(inputText);
+        this.add(okButton);
 
         okButton.addActionListener(e -> {
             label.setText(inputText.getText());
-            frame.remove(inputText);
-            frame.remove(okButton);
-            frame.add(label);
-            frame.revalidate(); //without this line, the inputBox still in the window
-            frame.repaint(); //this one will repaint the window, without it, the textbox removed still there, but without interaction
+            this.remove(inputText);
+            this.remove(okButton);
+            this.add(label);
+            this.revalidate(); //without this line, the inputBox still in the window
+            this.repaint(); //this one will repaint the window, without it, the textbox removed still there, but without interaction
         });
 
-        frame.setVisible(true);
+        this.getContentPane().setBackground(new Color(60,0,120));
+
+        ImageIcon image = new ImageIcon("/assets/image.png");
+        this.setIconImage(image.getImage());
+        this.setVisible(true);
     }
 }
