e896942cb25d460a68c6ddfa782db1578f91281c
kingabreel <gabrielyury441@gmail.com>
2023-09-15 12:07:38 -0300

    text inputed is showed to the user when the button is pressed

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -4,21 +4,34 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainWindow {
-    //Jframe is the main windows, it's constructor receive an parameter that will be the title of the window.
+    //frame is the main windows, it's constructor receive a parameter that will be the title of the window.
     public void window(){
         JFrame frame = new JFrame("First User Interface Program");
         frame.setSize(500, 200);
-        /*This method "setDefaultCloseOperation" will tell java what to do when the windows is closed, if you do not use
+        /*This method "setDefaultCloseOperation" will tell java what to do when the window is closed, if you do not use
         it, when you close the window the program will continue running.*/
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setLayout(new FlowLayout());
 
-        //JTextField will receive an user input. The argument size of the text field, each unit is the size of one caractere
+        //JTextField will receive a user input. The argument size of the text field, each unit is the size of one character
         JTextField inputText = new JTextField(30);
-        frame.add(inputText);
 
-        String label = inputText.getText();
+        JButton okButton = new JButton("Ok");
+
+        frame.add(inputText);
+        frame.add(okButton);
+
+        JLabel label = new JLabel();
+
+        okButton.addActionListener(e -> {
+            label.setText(inputText.getText());
+            frame.remove(inputText);
+            frame.remove(okButton);
+            frame.add(label);
+            frame.revalidate(); //without this line, the inputBox still in the window
+            frame.repaint(); //this one will repaint the window, without it, the textbox removed still there, but without interaction
+        });
 
         frame.setVisible(true);
     }
